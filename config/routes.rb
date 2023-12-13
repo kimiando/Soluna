a969f57846063294fffffefe34111bcfe869063e
kimiko ando <kimiko.ando13@icloud.com>
2023-12-14 03:29:15 +0900

    added destroy method for chatrooms (doesnt look pretty but it works)

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :chatrooms, only: [:index, :show, :new, :create] do
-    resources :messages, only: [:create, :new]
+  resources :chatrooms do
+    resources :messages, only: %i[create new destroy]
   end
   # Defines the root path route ("/")
   # root "chatrooms#show"
