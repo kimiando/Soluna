b413d34e8235d8afa8b673034853b7b387ab155a
kimiko ando <kimiko.ando13@icloud.com>
2023-12-14 03:05:20 +0900

    solved the migration problems and can now access the chatrooms

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,6 @@
 class ChatroomsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @chatrooms = Chatroom.all
   end
@@ -6,5 +8,28 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @messages = @chatroom.messages
+  end
+
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = current_user.chatrooms.build(chatroom_params)
+    @message = Message.new
+
+    if @chatroom.save
+      redirect_to @chatroom, notice: 'Chatroom was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
