c0d566df18ca613ed397529b711cd54327039c42
kimiko ando <kimiko.ando13@icloud.com>
2024-01-01 14:45:44 +0900

    added a new migration to rename the user-form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # def create
-  #   @user = User.find(params[:id])
-  # end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_31_120107) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_01_054143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,9 +69,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_31_120107) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "firstName"
-    t.string "lastName"
-    t.string "userName"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
     t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
