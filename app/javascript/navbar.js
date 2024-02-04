38826675d03080c4292b8af7f7785cf28427b99d
kimiko ando <kimiko.ando13@icloud.com>
2024-02-04 22:16:17 +0900

    changed the navbar and created a simple landing page

---
+++ app/javascript/navbar.js
@@ -0,0 +1,28 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello from Rails 7!');
+});
+
+var dropdownContainers = document.getElementsByClassName("dropdown-container");
+
+for (var i = 0; i < dropdownContainers.length; i++) {
+  dropdownContainers[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.querySelector('.dropdown-content');
+
+    var displayStyle = window.getComputedStyle(dropdownContent).getPropertyValue('display');
+
+    if (displayStyle === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
