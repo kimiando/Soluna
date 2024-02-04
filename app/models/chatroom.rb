f0ccfde8cfcba20e95e6479c8fd5fd0041a42b5d
kimiko ando <kimiko.ando13@icloud.com>
2024-02-05 04:36:32 +0900

    updated the messaging css

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,7 @@
 class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :users
+  belongs_to :pages
   has_one_attached :photo
   # has_many :users, through: :messages
 end
