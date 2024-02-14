7ac4c73af5fa532f68424dc83ef016748b660307
kimiko ando <kimiko.ando13@icloud.com>
2024-02-14 19:39:48 +0900

    changed models because there are problems during deployment not sure if the issue is fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :messages
-  has_many :chatrooms
+  # has_many :chatrooms
+  has_many :chatrooms, through: :messages
   has_one_attached :photo
   validates :first_name, presence: true
   validates :last_name, presence: true
